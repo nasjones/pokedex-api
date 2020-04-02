a526ca3675241e5fe165e909d95516a4fe56991e
nasjones <nasjonez1@gmail.com>
2020-04-01 21:30:28 -0400

    token fix

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN
 
 
-    if (!authToken || authToken !== apiToken) {
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
         return res.status(401).json({ error: 'Unauthorized request' })
     }
     // move to the next middleware
